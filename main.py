e0e2bc8bee8b50f16f4dc05e694b6b378031367c
byronbrown <68286443+byronbrown@users.noreply.github.com>
2020-07-22 16:39:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+a=Dogs nose are wet to absorb scent chemicals
+b=Newfoundlands are amazing lifeguards
+c=The Beatles song A Day in the Life has a frequency only dogs can hear.
+d=A Bloodhounds sense of smell can be used as evidence in court.
+#if user inputs num 1 print a
+a=int(input)
+if a=1 
+print"Dogs noses are wet to  absorb scent chemicals" elif a=1
